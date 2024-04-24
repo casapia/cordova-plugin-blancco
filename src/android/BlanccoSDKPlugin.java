702e0777d57f3a59684424ef20b7645cdbd861ba
Casapia <angel.casapia@gmail.com>
2024-04-24 16:18:24 +0200

    feat: Add Blancco SDK plugin files and dependencies

--- src/android/BlanccoSDKPlugin.java
+++ src/android/BlanccoSDKPlugin.java
@@ -1,13 +1,73 @@
 package cl.entel.plugins;
 
-import android.util.Log;
+import static by.chemerisuk.cordova.support.ExecutionThread.WORKER;
+
+import android.os.Build;
+
+import com.inhancetechnology.framework.hub.Hub;
+import com.inhancetechnology.healthchecker.ui.plays.TestSequencePlay;
+import com.inhancetechnology.healthchecker.ui.plays.enums.TestType;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaArgs;
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import by.chemerisuk.cordova.support.CordovaMethod;
 import by.chemerisuk.cordova.support.ReflectiveCordovaPlugin;
+import timber.log.Timber;
 
 public class BlanccoSDKPlugin extends ReflectiveCordovaPlugin {
     private static final String TAG = "BlanccoSDKPlugin";
 
     @Override
     protected void pluginInitialize() {
-        Log.d(TAG, "Starting Blancco SDK plugin");
+        Timber.tag(TAG).d("Starting Blancco SDK plugin");
+    }
+
+    @CordovaMethod(WORKER)
+    public void activate(final CordovaArgs args, final CallbackContext callbackContext) {
+        Timber.tag(TAG).d("Activating Blancco SDK");
+        cordova.getThreadPool().execute(new Runnable() {
+            public void run() {
+                try {
+                    String key = args.getString(0).trim();
+                    if (key.isEmpty()) {
+                        throw new IllegalArgumentException("API key is required");
+                    }
+                    Hub.getInstance(cordova.getContext()).activateApiKey(key);
+                    callbackContext.success();
+                } catch (Exception e) {
+                    Timber.tag(TAG).e(e, "Error activating Blancco SDK");
+                    callbackContext.error(e.getMessage());
+                }
+            }
+        });
+    }
+
+    @CordovaMethod(WORKER)
+    public void runSequence(final CordovaArgs args, final CallbackContext callbackContext) {
+        Timber.tag(TAG).d("Running Blancco SDK sequence");
+        cordova.getThreadPool().execute(new Runnable() {
+            public void run() {
+                try {
+                    JSONArray jsonArray = args.getJSONArray(0);
+                    List<TestType> tests = new ArrayList<>();
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        tests.add(TestType.values()[jsonArray.getInt(i)]);
+                    }
+
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                        TestSequencePlay.runSequence(cordova.getContext(), tests);
+                    }
+                    callbackContext.success();
+                } catch (Exception e) {
+                    Timber.tag(TAG).e(e, "Error activating Blancco SDK");
+                    callbackContext.error(e.getMessage());
+                }
+            }
+        });
     }
 }
--- www/BlanccoSDK.js
+++ www/BlanccoSDK.js
@@ -1,3 +1,43 @@
 var PLUGIN_NAME = "BlanccoSDKPlugin";
 // @ts-ignore
 var exec = require("cordova/exec");
+
+exports.activate =
+  /**
+   * Activates the Blancco SDK
+   *
+   * @param {string} key The Blancco SDK key
+   * @returns {Promise<void>} Callback when operation is completed
+   *
+   * @example
+   * cordova.plugins.BlanccoSDK(key).activate().then(() => {
+   *    console.log("Blancco SDK activated");
+   * }).catch((error) => {
+   *   console.error("Error activating Blancco SDK", error);
+   * });
+   */
+  function (key) {
+    return new Promise(function (resolve, reject) {
+      exec(resolve, reject, PLUGIN_NAME, "activate", [key]);
+    });
+  };
+
+exports.runSequence =
+  /**
+   * Runs a Blancco SDK sequence
+   *
+   * @param {Array<number>} sequence The sequence to run
+   * @returns {Promise<void>} Callback when operation is completed
+   *
+   * @example
+   * cordova.plugins.BlanccoSDK.runSequence([1, 2, 3]).then(() => {
+   *   console.log("Blancco SDK sequence run");
+   * }).catch((error) => {
+   *  console.error("Error running Blancco SDK sequence", error);
+   * });
+   */
+  function (sequence) {
+    return new Promise(function (resolve, reject) {
+      exec(resolve, reject, PLUGIN_NAME, "runSequence", [sequence]);
+    });
+  };
